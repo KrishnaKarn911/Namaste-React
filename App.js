69d74dc8783ec23276211ceeb6fc813bc805c2d0
Krishna <kthimmaraju@gammastack.com>
2025-03-19 23:56:03 +0530

    react components, elements and jsx class fundamentals

--- App.js
+++ App.js
@@ -2,6 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
-const heading = React.createElement("h1", {}, "Hello from React");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+const description = <h3>Above are the fruits list</h3>
+const bananaQuantity = 10
+const appleQuantity = 5
+const pineAppleQuantity = 6
+const orangeQuantity = 7
+const FruitListComponent = () => {
+    return (<div>
+        <div id="container">
+            <li>Banana</li><p>{bananaQuantity}kg</p>
+            <li>Apple</li><p>{appleQuantity}kg</p>
+            <li>Orange</li><p>{orangeQuantity}kg</p>
+            <li>PineApple</li><p>{pineAppleQuantity}kg</p>
+        </div>
+        {description}
+    </div>)
+}
+const Heading = () => (<div><h1>List of Fruits and their quantity</h1>
+    <FruitListComponent /></div>
+)
+
+
+
+root.render(<Heading />);
